1a3e259ed5a4d3a0992c6a506a7bf228a202e132
Boris Brönner <borisbroenner@hintergarten.de>
2024-09-21 11:57:08 +0200

    introduce app class

---
+++ app/app.go
@@ -0,0 +1,96 @@
+package app
+
+import (
+	"context"
+
+	"media-nexus/adapters/primary/ahttp"
+	"media-nexus/adapters/secondary/aaws"
+	"media-nexus/adapters/secondary/amongodb"
+	"media-nexus/config"
+	"media-nexus/errortypes"
+	"media-nexus/logger"
+	"media-nexus/ports"
+	"media-nexus/services"
+	"media-nexus/util"
+
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type App interface {
+	Setup() error
+	Run() error
+
+	TagRepo() ports.TagRepository
+}
+
+func NewApp(log logger.Logger, config *config.Configuration) App {
+	return &app{
+		log:    log,
+		config: config,
+	}
+}
+
+type app struct {
+	log    logger.Logger
+	config *config.Configuration
+
+	runners      []util.Runner
+	mediaService services.MediaService
+	tagRepo      ports.TagRepository
+}
+
+func (a *app) Setup() error {
+	ctx := util.WithLogger(context.Background(), a.log)
+
+	a.log.Info("setting up services ...")
+
+	awsConfig, err := awsconfig.LoadDefaultConfig(ctx)
+	if err != nil {
+		return errortypes.NewIllegalStatef("failed to load aws config: %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(awsConfig)
+	presignClient := s3.NewPresignClient(s3Client)
+
+	mongodbClient, err := amongodb.NewMongoDBClient(ctx, a.config.MongoDBURI)
+	if err != nil {
+		return errortypes.NewIllegalStatef("failed to create mongodb client: %v", err)
+	}
+
+	a.tagRepo = amongodb.NewTagRepository(mongodbClient, a.config.MediaDatabase, a.config.MediaTagCollection)
+	mediaMetadata, mediaMetadataRunner := amongodb.NewMediaMetadataRepository(
+		mongodbClient,
+		a.config.MediaDatabase,
+		a.config.MediaMetadataCollection,
+		a.config.IncompleteMediaMetadataLifetime,
+	)
+	a.runners = append(a.runners, mediaMetadataRunner)
+
+	media := aaws.NewMediaRepository(s3Client, presignClient, a.config.MediaBucket)
+	a.mediaService = services.NewMediaService(
+		a.tagRepo,
+		mediaMetadata,
+		media,
+		a.config.MediaUrlLifetime,
+		a.config.IncompleteMediaMetadataLifetime,
+	)
+
+	return nil
+}
+
+func (a *app) Run() error {
+	ctx := util.WithLogger(context.Background(), a.log)
+
+	a.log.Info("starting async tasks ...")
+	for _, runner := range a.runners {
+		go runner(ctx)
+	}
+
+	a.log.Info("starting API ...")
+	return ahttp.StartAPI(a.log, a.config.BaseUrl, a.config.HTTPPort, a.mediaService, a.tagRepo)
+}
+
+func (a *app) TagRepo() ports.TagRepository {
+	return a.tagRepo
+}
--- main.go
+++ main.go
@@ -1,90 +1,35 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"os"
 
-	"media-nexus/adapters/primary/ahttp"
-	"media-nexus/adapters/secondary/aaws"
-	"media-nexus/adapters/secondary/amongodb"
+	"media-nexus/app"
 	"media-nexus/config"
-	"media-nexus/errortypes"
 	"media-nexus/logger"
-	"media-nexus/services"
-	"media-nexus/util"
-
-	awsconfig "github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-var log = logger.NewLogger("main")
-
 func main() {
-	envPrefix := "MEDIANEXUS"
+	var log = logger.NewLogger("main")
 
-	err := run(log, envPrefix)
-	if err != nil {
+	if err := run(log); err != nil {
 		log.Fatalf("%+v", err)
 		os.Exit(1)
 	}
 }
 
-func run(log logger.Logger, envPrefix string) error {
-	ctx := util.WithLogger(context.Background(), log)
-
-	flag.Parse()
-
+func run(log logger.Logger) error {
 	log.Infof("Reading config ...")
 
-	if flag.NArg() < 1 {
-		return errortypes.NewBadUserInput("Provide a path to the config file.")
-	}
-
-	config, err := config.ParseConfigurationFile(flag.Arg(0), envPrefix)
+	config, err := config.LoadConfiguration()
 	if err != nil {
 		return err
 	}
 
-	log.Info("setting up services ...")
+	application := app.NewApp(log, config)
 
-	var runners []util.Runner
-
-	awsConfig, err := awsconfig.LoadDefaultConfig(ctx)
-	if err != nil {
-		return errortypes.NewIllegalStatef("failed to load aws config: %v", err)
+	if err := application.Setup(); err != nil {
+		return err
 	}
 
-	s3Client := s3.NewFromConfig(awsConfig)
-	presignClient := s3.NewPresignClient(s3Client)
-
-	mongodbClient, err := amongodb.NewMongoDBClient(ctx, config.MongoDBURI)
-	if err != nil {
-		return errortypes.NewIllegalStatef("failed to create mongodb client: %v", err)
-	}
-
-	tags := amongodb.NewTagRepository(mongodbClient, config.MediaDatabase, config.MediaTagCollection)
-	mediaMetadata, mediaMetadataRunner := amongodb.NewMediaMetadataRepository(
-		mongodbClient,
-		config.MediaDatabase,
-		config.MediaMetadataCollection,
-	)
-	runners = append(runners, mediaMetadataRunner)
-
-	media := aaws.NewMediaRepository(s3Client, presignClient, config.MediaBucket)
-	mediaService := services.NewMediaService(
-		tags,
-		mediaMetadata,
-		media,
-		config.MediaUrlLifetime,
-		config.IncompleteMediaMetadataLifetime,
-	)
-
-	log.Info("starting async tasks ...")
-	for _, runner := range runners {
-		go runner(ctx)
-	}
-
-	log.Info("starting API ...")
-	return ahttp.StartAPI(log, config.BaseUrl, config.HTTPPort, mediaService, tags)
+	return application.Run()
 }
